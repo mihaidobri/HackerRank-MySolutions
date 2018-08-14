7f29ff79633d081fef469e61b2356b9a03653f73
Mihai Dobri <dobrimihai@yahoo.com>
2018-08-14 08:01:27 +0300

    added Day12 Inheritence problem fix2

--- src/Day12Inheritance/Student.java
+++ src/Day12Inheritance/Student.java
@@ -1,36 +1,35 @@
 package Day12Inheritance;
 
 public class Student extends Person {
-private int[] testScores;
 
-    Student(String firstName, String lastName, int idNumber, int[] testScores ){
-        super(firstName,lastName,idNumber);
-        this.testScores=testScores;
+    private int[] testScores;
+
+    Student(String firstName, String lastName, int id, int[] testScores) {
+        super(firstName, lastName, id);
+        this.testScores = testScores;
     }
-    int n=0;
 
-    char avg;
-
+    int avg = 0;
     public char calculate() {
-        for(int i=0; i<testScores.length; i++){
-            n++;
-            avg = (char) ((testScores[i]+avg)/n);
-        }
 
-        if (avg >= 90 && avg <= 100) {
-            avg = 'O';
-            return avg;
-        } else if (avg <= 80 && avg < 90) {
-            return 'E';
-        } else if (avg <= 70 && avg < 80) {
-            return 'A';
-        } else if (avg <= 55 && avg < 70) {
-            return 'P';
-        } else if (avg <= 40 && avg < 55) {
-            return 'D';
-        } else if (avg < 40) {
-            return 'T';
+        for (int i = 0; i < testScores.length; i++) {
+
+            avg += testScores[i];
         }
-        return avg;
+        avg = avg / testScores.length;
+
+        if (avg >= 90) {
+            return '0';
+        } else if (avg >= 80) {
+            return 'E';
+        } else if (avg >= 70) {
+            return 'A';
+        } else if (avg > 55) {
+            return 'P';
+        } else if (avg >= 40) {
+            return 'D';
+        } else
+            return 'T';
+
     }
 }
