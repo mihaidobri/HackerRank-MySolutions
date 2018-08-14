490f926df5a2aac3e8a96b08215fb30ee4626e0e
mihaidobri <dobrimihai@yahoo.com>
2018-08-14 07:05:03 +0300

    added Day12 Inheritence problem fix

--- src/Day12Inheritance/Student.java
+++ src/Day12Inheritance/Student.java
@@ -10,13 +10,27 @@ private int[] testScores;
     int n=0;
 
     char avg;
-    public int calculate(){
+
+    public char calculate() {
         for(int i=0; i<testScores.length; i++){
             n++;
             avg = (char) ((testScores[i]+avg)/n);
         }
 
+        if (avg >= 90 && avg <= 100) {
+            avg = 'O';
+            return avg;
+        } else if (avg <= 80 && avg < 90) {
+            return 'E';
+        } else if (avg <= 70 && avg < 80) {
+            return 'A';
+        } else if (avg <= 55 && avg < 70) {
+            return 'P';
+        } else if (avg <= 40 && avg < 55) {
+            return 'D';
+        } else if (avg < 40) {
+            return 'T';
+        }
         return avg;
     }
-
 }
