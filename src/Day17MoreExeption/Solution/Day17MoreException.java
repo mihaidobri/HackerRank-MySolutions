d0c574846065b06dbe31604bff1b9d61e18d14cb
Mihai Dobri <dobrimihai@yahoo.com>
2018-08-16 20:00:40 +0300

    added Day17 More Exception Solution fix

--- src/Day17MoreExeption/Solution/Day17MoreException.java
+++ src/Day17MoreExeption/Solution/Day17MoreException.java
@@ -2,6 +2,13 @@ package Day17MoreExeption.Solution;
 
 import java.util.Scanner;
 
+/**
+ * Task
+ * Write a Calculator class with a single method: int power(int,int).
+ * The power method takes two integers, p and n, as parameters and returns the integer result of n^p.
+ * If either n or p is negative, then the method must throw an exception with the message:
+ * "n and p should be non-negative."
+ */
 class Calculator {
     int m = 1;
 
@@ -24,9 +31,11 @@ class Day17MoreException {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
+        System.out.println("For how many numbers we should calculate the power of n ");
         int t = in.nextInt();
         while (t-- > 0) {
 
+            System.out.println("Enter your \"n\" followed by a \"space\" and your \"p\" ");
             int n = in.nextInt();
             int p = in.nextInt();
             Calculator myCalculator = new Calculator();
