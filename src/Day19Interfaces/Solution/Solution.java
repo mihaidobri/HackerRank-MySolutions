8a7ff6d80fe72fa3d8d909b61a061c862bfea4b2
Mihai Dobri <dobrimihai@yahoo.com>
2018-08-17 18:57:27 +0300

    added Day19 Solution fix

--- src/Day19Interfaces/Solution/Solution.java
+++ src/Day19Interfaces/Solution/Solution.java
@@ -2,10 +2,15 @@ package Day19Interfaces.Solution;
 
 import java.util.Scanner;
 
+/*
+ *Complete the implementation of Day19Class class, which implements the AdvancedArithmetic interface.
+ *The implementation for the divisorSum(n) method must return the sum of all divisors of n.
+ */
 public class Solution {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number you want to compute its divisors");
         int n = scan.nextInt();
         scan.close();
 
